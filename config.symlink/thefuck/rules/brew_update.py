485a7b70bbef2a59a0e417f804f03dda92e9c8c7
Jeff Koenig <jkoenig311@gmail.com>
2017-04-27 14:54:36 -0400

    Update `thefuck`

--- config.symlink/thefuck/rules/brew_update.py
+++ config.symlink/thefuck/rules/brew_update.py
@@ -2,15 +2,13 @@ patterns = [
 'This command updates brew itself, and does not take formula names.'
             ]
 
-
-def match(command, settings):
+def match(command):
     for pattern in patterns:
         if pattern.lower() in command.stderr.lower():
             return True
     return False
 
-
-def get_new_command(command, settings):
+def get_new_command(command):
     array = command.script.split(" ")
     index = array.index('update')+1
     return 'brew upgrade ' + array[index]
